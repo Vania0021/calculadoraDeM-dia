752bfea456a994158f0e6d29d122acd786f8d4e9
Vânia <89160063+Vania0021@users.noreply.github.com>
2021-09-15 21:55:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var nota1 = 10
+var nota2 = 10
+var nota3 = 10
+var nota4 = 5
+var media
+
+media = (nota1 + nota2 + nota3 + nota4) / 4
+console.log ("Notas: " + nota1 + ", " + nota2 + ", "+ nota3 + ", "+ nota4)
+console.log ("MEDIA = " + media);
